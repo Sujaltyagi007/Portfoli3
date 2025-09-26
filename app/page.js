b95f6a053c7237be3ac1443233ed3156b5e9a03d
Sujal07 <Sujal07@gmail.com>
2025-09-26 14:37:37 +0530

    ready to deploy

--- app/page.js
+++ app/page.js
@@ -3,9 +3,16 @@ import About from "@/components/pages/About";
 import Home from "@/components/pages/Home";
 import Work from "@/components/pages/Work";
 import { useTabContext } from "@/context/TabContext";
+import { Fragment } from "react";
 
-export default function page() {
-  const Tabs = [<Home />, <About />, <Work />, <h1>Contact</h1>, <h1>Blog</h1>];
+export default function Page() {
+  const Tabs = [
+    { tab: <Home /> },
+    { tab: <About /> },
+    { tab: <Work /> },
+    { tab: <h1>Contact</h1> },
+    { tab: <h1>Blog</h1> },
+  ];
   const { activeTab } = useTabContext();
-  return Tabs[activeTab];
+  return <Fragment key={activeTab}>{Tabs[activeTab].tab}</Fragment>;
 }
